112744b6cae7809394ee4711d5e1cb49598ec7ea
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-06-23 16:05:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+
+ let port = 3000;
+
+   app.set("view engine","ejs");
+
+   app.listen(port,()=>{
+      console.log("server is runing on port ", port);
+   });
+    
+    app.get("/",(req,res)=> {
+        console.log("request Recived ")
+       res.render("home.ejs");
+    })
+
+    app.get("/hello",(req,res)=> {
+       res.send("Hello");
+    });
+
+    app.get("/rolldice",(req,res)=>{
+      let diceval = Math.floor(Math.random()*6)+1;
+       res.render("rolldice.ejs",{num:diceval});
+    })
